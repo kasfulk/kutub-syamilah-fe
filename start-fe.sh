5c2d7fa0649f25410c2c85475756128ad30806f1
Kasjful Kurniawan <kasful.bpp@gmail.com>
2026-04-20 12:30:33 +0800

    chore: remove dependency installation and build steps from startup script

--- start-fe.sh
+++ start-fe.sh
@@ -4,14 +4,5 @@ export PORT=3300
 export NEXT_PUBLIC_KUTUB_API=https://kutub.litensweet.id/api
 export NEXT_PUBLIC_SITE_URL=https://kutub.litensweet.id
 
-echo "Installing dependencies..."
-npm install
-
-echo "Cleaning previous build..."
-rm -rf .next
-
-echo "Building Next.js..."
-npm run build
-
 echo "Starting Next.js..."
 npm start -- -p 3300
